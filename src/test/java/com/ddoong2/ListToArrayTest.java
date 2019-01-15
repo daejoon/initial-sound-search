2b302f8a812e5688692c894bcc8a9dfaf4b5e07e
daejoon <kkode1911@gmail.com>
2019-01-15 23:38:15 +0900

    개발을 하다가 중간에 API를 테스트 하고 싶을때는 Unit Test를 이용하면 좋다.

---
+++ src/test/java/com/ddoong2/ListToArrayTest.java
@@ -0,0 +1,47 @@
+package com.ddoong2;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+public class ListToArrayTest {
+
+    @Test
+    public void 리스트를_배열로_변환한때_인자로_넘기는_개수가_많으면_그_개수만큼_배열이_변환된다() {
+        List<String> data = new ArrayList<>();
+        data.add("A");
+        data.add("B");
+        data.add("C");
+
+        assertThat(data.toArray(new String[5]).length, is(5));
+    }
+
+    @Test
+    public void 리스트를_배열로_변환한때_인자로_넘기는_개수가_리스트_개수보다_작다면_리스트의_사이즈만큼_배열이_변환된다() {
+        List<String> data = new ArrayList<>();
+        data.add("A");
+        data.add("B");
+        data.add("C");
+
+        assertThat(data.toArray(new String[2]).length, is(3));
+    }
+
+    @Test
+    public void 리스트를_배열로_변환한때_인자로_넘기는_개수가_많으면_그_나머지는_NULL로_채워진다() {
+        List<String> data = new ArrayList<>();
+        data.add("A");
+        data.add("B");
+        data.add("C");
+
+        String[] array = data.toArray(new String[5]);
+
+        assertThat(array[2], is("C"));
+        assertThat(array[3], is(nullValue()));
+        assertThat(array[4], is(nullValue()));
+    }
+}
