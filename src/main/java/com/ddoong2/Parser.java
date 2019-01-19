28b0619ae1c62e410ed1e591d7ff808cb4ee216a
daejoon <kkode1911@gmail.com>
2019-01-19 12:36:13 +0900

    Refactoring Parser.java

--- src/main/java/com/ddoong2/Parser.java
+++ src/main/java/com/ddoong2/Parser.java
@@ -10,29 +10,29 @@ public class Parser {
     private static final int SYLLABLE_END_CODE = 0xD7A3;
     private static final int CYCLE = 588;
 
-    private final Map<Integer, Integer> compatibleInitial = new HashMap<>();
+    private final Map<Character, Character> compatibleInitial = new HashMap<>();
 
     public Parser() {
-        int code = 0x1100;
-        compatibleInitial.put("ㄱ".codePointAt(0), code++);
-        compatibleInitial.put("ㄲ".codePointAt(0), code++);
-        compatibleInitial.put("ㄴ".codePointAt(0), code++);
-        compatibleInitial.put("ㄷ".codePointAt(0), code++);
-        compatibleInitial.put("ㄸ".codePointAt(0), code++);
-        compatibleInitial.put("ㄹ".codePointAt(0), code++);
-        compatibleInitial.put("ㅁ".codePointAt(0), code++);
-        compatibleInitial.put("ㅂ".codePointAt(0), code++);
-        compatibleInitial.put("ㅃ".codePointAt(0), code++);
-        compatibleInitial.put("ㅅ".codePointAt(0), code++);
-        compatibleInitial.put("ㅆ".codePointAt(0), code++);
-        compatibleInitial.put("ㅇ".codePointAt(0), code++);
-        compatibleInitial.put("ㅈ".codePointAt(0), code++);
-        compatibleInitial.put("ㅉ".codePointAt(0), code++);
-        compatibleInitial.put("ㅊ".codePointAt(0), code++);
-        compatibleInitial.put("ㅋ".codePointAt(0), code++);
-        compatibleInitial.put("ㅌ".codePointAt(0), code++);
-        compatibleInitial.put("ㅍ".codePointAt(0), code++);
-        compatibleInitial.put("ㅎ".codePointAt(0), code++);
+        char code = 0x1100;
+        compatibleInitial.put('ㄱ', code++);
+        compatibleInitial.put('ㄲ', code++);
+        compatibleInitial.put('ㄴ', code++);
+        compatibleInitial.put('ㄷ', code++);
+        compatibleInitial.put('ㄸ', code++);
+        compatibleInitial.put('ㄹ', code++);
+        compatibleInitial.put('ㅁ', code++);
+        compatibleInitial.put('ㅂ', code++);
+        compatibleInitial.put('ㅃ', code++);
+        compatibleInitial.put('ㅅ', code++);
+        compatibleInitial.put('ㅆ', code++);
+        compatibleInitial.put('ㅇ', code++);
+        compatibleInitial.put('ㅈ', code++);
+        compatibleInitial.put('ㅉ', code++);
+        compatibleInitial.put('ㅊ', code++);
+        compatibleInitial.put('ㅋ', code++);
+        compatibleInitial.put('ㅌ', code++);
+        compatibleInitial.put('ㅍ', code++);
+        compatibleInitial.put('ㅎ', code++);
     }
 
     public boolean find(String search, String source) {
@@ -87,15 +87,15 @@ public class Parser {
     private char getCharToInitialSound(char syllable) {
         if (isHangul(syllable)) {
             return ((char) (CONSONANT_START_CODE + ((syllable - SYLLABLE_START_CODE) / CYCLE)));
-        } else if ( isInputCompatibleInitialSound(syllable) ) {
-            return ((char) compatibleInitial.get(Integer.valueOf(syllable)).intValue());
+        } else if ( isCompatibleHangul(syllable) ) {
+            return compatibleInitial.get(syllable).charValue();
         }
 
         return syllable;
     }
 
-    private boolean isInputCompatibleInitialSound(char syllable) {
-        return compatibleInitial.containsKey(Integer.valueOf(syllable));
+    private boolean isCompatibleHangul(char syllable) {
+        return compatibleInitial.containsKey(syllable);
     }
 
     private boolean isHangul(char syllable) {
